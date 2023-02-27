323a294823b26ddf052241b0dae90a30a468c3d7
dryerem <mamayma8@gmail.com>
2023-02-27 10:53:08 +0300

    Добавьте файлы проекта.

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,43 @@
+﻿namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public static int GetQuantityForProduct(int productType, int materialType, 
+                                         int count, float width, float length)
+        {
+            if (count <= 0 || width <= 0 || length <= 0) return -1;
+
+            double coefficient;
+            switch (productType)
+            {
+                case 1:
+                    coefficient = 1.1;
+                    break;
+                case 2:
+                    coefficient = 2.5;
+                    break;
+                case 3:
+                    coefficient = 8.43;
+                    break;
+                default:
+                    return -1;
+            }
+
+            double percentage;
+            switch (materialType)
+            {
+                case 1:
+                    percentage = 0.003;
+                    break;
+                case 2:
+                    percentage = 0.0012;
+                    break;
+                default:
+                    return -1;
+            }
+
+            double required = (width * length) * count * coefficient / (1 - percentage);
+            return (int)Math.Ceiling(required);
+        }
+    }
+}
---
+++ WSUniversalLibTests/CalculationTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace WSUniversalLib.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProduct_ForCorrectParameters()
+        {
+            int expected = 114148;
+            int actual = Calculation.GetQuantityForProduct(3, 1, 15, 20, 45);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProduct_ForUnknownProductType()
+        {
+            int expected = -1;
+            int actual = Calculation.GetQuantityForProduct(4, 2, 5, 10, 20);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProduct_ForUnknownMaterialType()
+        {
+            int expected = -1;
+            int actual = Calculation.GetQuantityForProduct(3, 3, 3, 15, 25);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProduct_ForNullableCount()
+        {
+            int expected = -1;
+            int actual = Calculation.GetQuantityForProduct(1, 1, 0, 10, 10);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProduct_ForNegativeProductCount()
+        {
+            int expected = -1;
+            int actual = Calculation.GetQuantityForProduct(3, 1, -5, 20, 45);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetQuantityForProduct_ForNullableArea()
+        {
+            int expected = -1;
+            int actual = Calculation.GetQuantityForProduct(2, 2, 10, 0, 0);
+            Assert.AreEqual(expected, actual); ;
+        }
+    }
+}
